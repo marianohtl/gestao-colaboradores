392fe94cde5cd6fef51cd7f3bd5fca0c9e71ea92
thapita <8.litah.8@gmail.com>
2025-09-14 19:26:28 -0300

    include spring initiazilr base project

---
+++ src/main/java/br/com/gestao_colaboradores_api/GestaoColaboradoresApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.testebackendPostgreSQL.com.Flyway.ou.Liquibase.gestao_colaboradores_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoColaboradoresApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoColaboradoresApiApplication.class, args);
+	}
+
+}
